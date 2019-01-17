217f639a0d75af0102f4f560353deba904b347ae
Sivarjuen Ravichandran <sxr549@student.bham.ac.uk>
2019-01-17 00:28:48 +0000

    Created remaining wall assets

---
+++ Test/SliderImageFill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderImageFill : MonoBehaviour {
+
+	[SerializeField]
+	private Slider slider;
+	private Image image;
+
+
+	private void Awake() {
+		image = GetComponent<Image>();
+		slider.onValueChanged.AddListener(HandleSliderChanged);
+	}
+
+	// Use this for initialization
+	private void Start () {
+		HandleSliderChanged(slider.value);
+	}
+
+	private void HandleSliderChanged(float value){
+		image.fillAmount = value;
+	}
+}
