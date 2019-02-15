9f744c42be06a30864afaada9b348311cdd78460
Sivarjuen Ravichandran <sxr549@student.bham.ac.uk>
2019-02-15 19:18:36 +0000

    Added level 2 features + script for lights out puzzle

---
+++ Scripts/Level2/PowerButton.cs
@@ -0,0 +1,77 @@
+﻿using Leap.Unity;
+using Leap.Unity.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InteractionBehaviour))]
+public class PowerButton : MonoBehaviour {
+
+	private bool on;
+	public bool startOn = false;
+	private Material _material;
+	private Color onColor = Color.green;
+	private Color offColor;
+	private Color targetColor;
+	private InteractionBehaviour _intObj;
+	private float toggleDelay = .2f;
+	private float toggleTimer = 0;
+	private PowerButton[] adjacentButtons;
+
+	void Start () {
+		on = startOn;
+		 _intObj = GetComponent<InteractionBehaviour>();
+
+		Renderer renderer = GetComponent<Renderer>();
+		if (renderer == null) {
+		renderer = GetComponentInChildren<Renderer>();
+		}
+		if (renderer != null) {
+		_material = renderer.material;
+		}
+		offColor = _material.color;
+		if(on){
+			targetColor = onColor;
+			_material.color = onColor;
+		} else {
+			targetColor = offColor;
+		}
+	}
+	
+	void Update () {
+		// Lerp actual material color to the target color.
+      	_material.color = Color.Lerp(_material.color, targetColor, 30F * Time.deltaTime);
+	}
+
+	private void toggle(){
+		if(Time.time - toggleTimer >= toggleDelay){
+			if(on){
+				on = false;
+				targetColor = offColor;
+			} else {
+				on = true;
+				targetColor = onColor;
+			}
+			toggleTimer = Time.time;
+		}
+	}
+
+	private void autoToggle(){
+		toggle();
+	}
+
+	public void manualToggle(){
+		toggle();
+		foreach(PowerButton b in adjacentButtons){
+			b.autoToggle();
+		}
+	}
+
+	private bool isOn(){
+		return on;
+	}
+
+	public void setAdjacentButtons(PowerButton[] buttons){
+		this.adjacentButtons = buttons;
+	}
+}
---
+++ Scripts/Level2/PowerPuzzle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPuzzle : MonoBehaviour {
+
+	public PowerButton[] buttons;
+
+	void Start () {
+		if(buttons.Length == 9){
+			PowerButton[] adj1 = new PowerButton[2];
+			PowerButton[] adj2 = new PowerButton[3];
+			PowerButton[] adj3 = new PowerButton[2];
+			PowerButton[] adj4 = new PowerButton[3];
+			PowerButton[] adj5 = new PowerButton[4];
+			PowerButton[] adj6 = new PowerButton[3];
+			PowerButton[] adj7 = new PowerButton[2];
+			PowerButton[] adj8 = new PowerButton[3];
+			PowerButton[] adj9 = new PowerButton[2];
+
+			adj1[0] = buttons[1];
+			adj1[1] = buttons[3];
+			adj2[0] = buttons[0];
+			adj2[1] = buttons[2];
+			adj2[2] = buttons[4];
+			adj3[0] = buttons[1];
+			adj3[1] = buttons[5];
+			adj4[0] = buttons[0];
+			adj4[1] = buttons[4];
+			adj4[2] = buttons[6];
+			adj5[0] = buttons[1];
+			adj5[1] = buttons[3];
+			adj5[2] = buttons[5];
+			adj5[3] = buttons[7];
+			adj6[0] = buttons[2];
+			adj6[1] = buttons[4];
+			adj6[2] = buttons[8];
+			adj7[0] = buttons[3];
+			adj7[1] = buttons[7];
+			adj8[0] = buttons[4];
+			adj8[1] = buttons[6];
+			adj8[2] = buttons[8];
+			adj9[0] = buttons[5];
+			adj9[1] = buttons[7];
+
+			buttons[0].setAdjacentButtons(adj1);
+			buttons[1].setAdjacentButtons(adj2);
+			buttons[2].setAdjacentButtons(adj3);
+			buttons[3].setAdjacentButtons(adj4);
+			buttons[4].setAdjacentButtons(adj5);
+			buttons[5].setAdjacentButtons(adj6);
+			buttons[6].setAdjacentButtons(adj7);
+			buttons[7].setAdjacentButtons(adj8);
+			buttons[8].setAdjacentButtons(adj9);
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
